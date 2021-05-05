5a6aca3102bc6e33487fc8bf8c1a6419488d727e
Jackie Vaughn <jbvaughn283@gmail.com>
2021-05-04 22:21:53 -0700

    last try on buttons for tonight

--- mailer.php
+++ mailer.php
@@ -1,18 +1,136 @@
 <?php
-if($_POST["Message"]) {
-    mail("jbvaughn283@gmail.com", "Form to email message", $_POST["Message"], "From: an@email.address");
+
+$errors = [];
+
+if (!empty($_POST)) {
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+
+   if (empty($name)) {
+       $errors[] = 'Name is empty';
+   }
+
+   if (empty($email)) {
+       $errors[] = 'Email is empty';
+   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $errors[] = 'Email is invalid';
+   }
+
+   if (empty($message)) {
+       $errors[] = 'Message is empty';
+   }
+}
+<?php
+if (!empty($errors)) {
+   $allErrors = join('<br/>', $errors);
+   $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+}
+<?php
+
+$errors = [];
+$errorMessage = '';
+
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+    if (empty($errors)) {
+        $toEmail = 'example@example.com';
+        $emailSubject = 'New email from your contant form';
+        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
+
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+        $body = join(PHP_EOL, $bodyParagraphs);
+
+        if (mail($toEmail, $emailSubject, $body, $headers)) {
+            header('Location: thank-you.html');
+        } else {
+            $errorMessage = 'Oops, something went wrong. Please try again later';
+        }
+    } else {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    }
+}
+?>
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+require __DIR__ . '/vendor/autoload.php';
+
+$errors = [];
+$errorMessage = '';
+
+if (!empty($_POST)) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if (empty($name)) {
+        $errors[] = 'Name is empty';
+    }
+
+    if (empty($email)) {
+        $errors[] = 'Email is empty';
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = 'Email is invalid';
+    }
+
+    if (empty($message)) {
+        $errors[] = 'Message is empty';
+    }
+
+
+    if (!empty($errors)) {
+        $allErrors = join('<br/>', $errors);
+        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+    } else {
+        $mail = new PHPMailer();
+
+        // specify SMTP credentials
+        $mail->isSMTP();
+        $mail->Host = 'smtp.mailtrap.io';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'd5g6bc7a7dd6c7';
+        $mail->Password = '27f211b3fcad87';
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 2525;
+
+        $mail->setFrom($email, 'Mailtrap Website');
+        $mail->addAddress('piotr@mailtrap.io', 'Me');
+        $mail->Subject = 'New message from your website';
+
+        // Enable HTML if needed
+        $mail->isHTML(true);
+
+        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", nl2br($message)];
+        $body = join('<br />', $bodyParagraphs);
+        $mail->Body = $body;
+
+        echo $body;
+        if($mail->send()){
+
+            header('Location: thank-you.html'); // redirect to 'thank you' page
+        } else {
+            $errorMessage = 'Oops, something went wrong. Mailer Error: ' . $mail->ErrorInfo;
+        }
+    }
 }
 
-if($_POST["submit"]) {
-    $recipient="jbvaughn283@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["Name"];
-    $senderEmail=$_POST["Email"];
-    $message=$_POST["Message"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
+?>
