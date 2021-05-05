c05c5a6ac0655bd27765d28d050215349e3fcedf
Jackie Vaughn <jbvaughn283@gmail.com>
2021-05-04 22:11:58 -0700

    Buttons are cancer

--- mailer.php
+++ mailer.php
@@ -1,6 +1,18 @@
 <?php
 if($_POST["Message"]) {
-mail("john@johndoe.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: jane@janedoe.com");
+    mail("jbvaughn283@gmail.com", "Form to email message", $_POST["Message"], "From: an@email.address");
 }
-?>
+
+if($_POST["submit"]) {
+    $recipient="jbvaughn283@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["Name"];
+    $senderEmail=$_POST["Email"];
+    $message=$_POST["Message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
